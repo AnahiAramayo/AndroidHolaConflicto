897d582dd21877a36c3d5c3eef81b75449fcf146
Programador2 <programador2@ejemplo.org>
2017-09-29 13:58:46 -0300

    Programador 2 pone un comentario en MainActivity.java

--- app/src/main/java/com/example/androidholaconflicto/MainActivity.java
+++ app/src/main/java/com/example/androidholaconflicto/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // comentario añadido por el Programador 2
     }
 }
